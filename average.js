de29710e01b3ce2985be1b608a2fd2910876659c
Amit Krishna A <amit.ananthkumar@gmail.com>
2021-12-09 23:31:04 +0530

    style: Fix lint errors

--- average.js
+++ average.js
@@ -1,7 +1,7 @@
 const SENSOR_FAULTY_THRESHOLD = 0.25;
 
 function average(numbers) {
-  var noOfNans = 0;
+  let noOfNans = 0;
   const len = numbers.length;
 
   const total = numbers.reduce((total, no) => {
@@ -17,4 +17,4 @@ function average(numbers) {
   return effLen / len < SENSOR_FAULTY_THRESHOLD ? NaN : avg;
 }
 
-module.exports = { average };
+module.exports = {average};
--- test/average.test.js
+++ test/average.test.js
@@ -1,22 +1,23 @@
 const {expect} = require('chai');
 const {average} = require('../average');
 
-it('computes average of a list of numbers', ()=> {
+it('computes average of a list of numbers', () => {
   // floating point numbers cannot be compared for equality,
   // hence allowing a delta tolerance
   expect(average([1, 2, 3, 4])).to.be.approximately(2.5, 0.01);
 });
 
-it('reports the average as NaN on an empty list', ()=> {
+it('reports the average as NaN on an empty list', () => {
   expect(average([])).to.be.NaN;
 });
 
-it('ignores NaN in the input', ()=> {
+it('ignores NaN in the input', () => {
   expect(average([1, NaN, 2])).to.be.approximately(1.5, 0.01);
 });
 
-describe("Check if the function detects when the sensor is faulty", ()=>{
-  it('reports the average as NaN when more than 75% of the reading are NaN', ()=> {
-    expect(average([NaN, NaN, NaN, NaN, NaN, 1, NaN])).to.be.NaN;
-  })
-})
+describe('Check if the function detects when the sensor is faulty', () => {
+  it('reports the average as NaN when more than 75% of the reading are NaN',
+      () => {
+        expect(average([NaN, NaN, NaN, NaN, NaN, 1, NaN])).to.be.NaN;
+      });
+});
